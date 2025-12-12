603bacd5cebf1fc81644eb8b63f479a4fc72bd00
theenuka <theenukabandara@gmail.com>
2025-12-12 06:28:42 +0530

    remove

--- terraform/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "phoenix-booking-terraform-state"
-    key            = "phoenix-booking/terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "phoenix-booking-terraform-lock"
-    encrypt        = true
-  }
-}
